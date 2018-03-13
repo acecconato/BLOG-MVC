124c5f10cdaec52d5f66041cb888394d5c1598d3
Anthony Cecconato <antho.cecc@gmail.com>
2018-03-13 16:21:35 +0100

    Entity class created for hydrate entities

---
+++ model/Entity.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Model;
+
+abstract class Entity
+{
+    protected function hydrate(array $data) {
+        foreach ($data as $attribute => $value) {
+            $method = "set" . ucfirst($attribute);
+            if(is_callable([$this, $method])) {
+                $this->$method($value);
+            }
+        }
+    }
+}
