3e726af0ec5d3ad89f4d72c00b7da4f90ad6caca
Anthony Cecconato <antho.cecc@gmail.com>
2018-03-14 16:00:10 +0100

    It's working ! I get my 2 blog posts in the index

--- controller/IndexController.php
+++ controller/IndexController.php
@@ -2,13 +2,25 @@
 
 namespace Controller;
 
+use Model\Post;
 use Model\PostsManager;
 
 class IndexController
 {
+    private static $_posts = [];
+
     public function homepage()
     {
         $postsManager = new PostsManager();
-        $postsManager->getAllPosts();
+        $posts = $postsManager->getAllPosts();
+
+        foreach ($posts as $k => $v) {
+            self::$_posts[$k] = new Post($v);
+        }
+
+        foreach(self::$_posts as $k => $v) {
+            print_r($v);
+        }
+
     }
 }
