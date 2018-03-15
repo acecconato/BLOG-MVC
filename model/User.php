227e2684b81c1fcf86fdd79aea13d03e9a38d0f4
Anthony Cecconato <antho.cecc@gmail.com>
2018-03-15 19:10:33 +0100

    User class and UsersManager class created WIP

--- model/User.php
+++ model/User.php
@@ -4,5 +4,87 @@ namespace Model;
 
 class User extends Entity
 {
+    private $userId,
+            $permissionLevel,
+            $email,
+            $pseudo,
+            $password,
+            $creationDate;
 
+    public function __construct(array $data)
+    {
+        $this->hydrate($data);
+    }
+
+    public function getPermissionLevel()
+    {
+        return $this->permissionLevel;
+    }
+
+    public function setPermissionLevel($permissionLevel)
+    {
+        $permissionLevel = (int) $permissionLevel;
+        if($permissionLevel >= 0) {
+            $this->permissionLevel = $permissionLevel;
+        }
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $this->email = $email;
+        }
+    }
+
+    public function getPseudo()
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo($pseudo)
+    {
+        if(is_string($pseudo)) {
+            $this->pseudo = $pseudo;
+        }
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        if(is_string($password)) {
+            $this->password = password_hash($password, PASSWORD_DEFAULT);
+        }
+    }
+
+    public function getCreationDate()
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate($creationDate)
+    {
+        $this->creationDate = $creationDate;
+    }
+
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    public function setUserId($userId)
+    {
+        $userId = (int) $userId;
+        if($userId >= 0) {
+            $this->userId = $userId;
+        }
+    }
 }
--- model/UsersManager.php
+++ model/UsersManager.php
@@ -4,5 +4,85 @@ namespace Model;
 
 class UsersManager extends PDOFactory
 {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function getAllUsers()
+    {
+        $query = parent::$_dbh->prepare("
+            SELECT *, DATE_FORMAT(creationDate, '%d/%m/%y à %Hh%i') as creationDate
+            FROM users
+            ORDER BY user_id DESC
+        ");
+
+        $query->execute();
+        $result = $query->fetchAll();
+        $query->closeCursor();
+        return $result;
+    }
+
+    /**
+     * @param $user
+     * @return array
+     * Search an user by this ID, Pseudo or Email address
+     */
+    public function getUser($user)
+    {
+        $query = parent::$_dbh->prepare("
+            SELECT *, DATE_FORMAT(creationDate, '%d/%m/%y à %Hh%i') as creationDate
+            FROM users
+            WHERE user_id = :id 
+            OR LOWER(pseudo) LIKE LOWER(:pseudo)
+            OR LOWER(email) LIKE LOWER(:email)
+        ");
+
+        $query->bindValue(":id", $user, \PDO::PARAM_INT);
+        $query->bindValue(":pseudo", $user, \PDO::PARAM_STR);
+        $query->bindValue(":email", $user, \PDO::PARAM_STR);
+
+        $query->execute();
+        $result = $query->fetchAll();
+        $query->closeCursor();
+        return $result;
+    }
+
+    public function addUser(User $user)
+    {
+        $query = parent::$_dbh->prepare("
+            INSERT INTO users 
+            (email, pseudo, password) 
+            VALUES 
+            (:email, :pseudo, :password)
+        ");
+
+        $query->bindValue(":email", $user->getEmail(), \PDO::PARAM_STR);
+        $query->bindValue(":pseudo", $user->getPseudo(), \PDO::PARAM_STR);
+        $query->bindValue(":password", $user->getPassword(), \PDO::PARAM_STR);
+
+        return $result = $query->execute();
+    }
+
+    public function updateUser(User $user)
+    {
+
+    }
+
+    public function deleteUser($user)
+    {
+        $query = parent::$_dbh->prepare("
+            DELETE FROM users
+            WHERE user_id = :id
+            OR pseudo = :pseudo
+            OR email = :email
+        ");
+
+        $query->bindValue(":id", $user, \PDO::PARAM_INT);
+        $query->bindValue(":pseudo", $user, \PDO::PARAM_STR);
+        $query->bindValue(":email", $user, \PDO::PARAM_STR);
+
+        return $result = $query->execute();
+    }
 
 }
