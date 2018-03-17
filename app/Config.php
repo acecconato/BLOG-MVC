2b1ebbca8a666d22cdc0d48352b81be2ea926861
Anthony Cecconato <antho.cecc@gmail.com>
2018-03-17 02:42:00 +0100

    Refactoring the configuration structure to have something more independant

--- app/Config.php
+++ app/Config.php
@@ -2,25 +2,19 @@
 
 namespace App;
 
-/**
- * Class Config
- * @package App
- * Singleton configuration file
- */
 class Config
 {
     private $settings = [];
     private static $_instance;
-    private static $_dbh;
 
     protected function __construct()
     {
         if(is_null(self::$_instance)) {
-            $this->settings = require dirname(__DIR__) . "/config.php";
+            $this->settings = require dirname(__DIR__) . "/app/config/config.php";
         }
     }
 
-    protected static function getInstance()
+    public static function getInstance()
     {
         if(is_null(self::$_instance)) {
             self::$_instance = new Config();
@@ -28,7 +22,7 @@ class Config
         return self::$_instance;
     }
 
-    protected function get($key) {
+    public function get($key) {
         if(!isset($this->settings[$key])) {
             return null;
         }
--- app/config/config.php
+++ app/config/config.php
@@ -4,5 +4,7 @@ return array(
     "db_hostname"   => "localhost",
     "db_name"       => "perso_blog",
     "db_user"       => "root",
-    "db_password"   => ""
+    "db_password"   => "",
+
+    "default_picture_path"  => dirname(__DIR__)."/public/image/posts"
 );
