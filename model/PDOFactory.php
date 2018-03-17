f6e7c28b19e279fe8e26bfd7c3592b9b37ea3b30
Anthony Cecconato <antho.cecc@gmail.com>
2018-03-17 02:47:29 +0100

    Refactoring PDOFactory : He no longer manages the connection to the database and the code is cleaner

--- model/PDOFactory.php
+++ model/PDOFactory.php
@@ -6,30 +6,18 @@ use App\Config;
 
 abstract class PDOFactory
 {
-    /* @var \PDO $_dbh */
-    protected static $_dbh;
-
-    protected function __construct()
+    public static function create($type)
     {
-        self::DBConnect("mysql");
-    }
-
-    protected static function DBConnect($type)
-    {
-        if(is_null(self::$_dbh)) {
-            $cfg = Config::getInstance();
-            try {
-                switch($type) {
-                    case "mysql":
-                        self::$_dbh = new \PDO("mysql:host=" . $cfg->get('db_hostname') . ";dbname=" . $cfg->get('db_name'), $cfg->get('db_user'), $cfg->get('db_password'));
-                        break;
-                    default:
-                        throw new \PDOException("Database type not found");
-                }
-            } catch (\PDOException $e) {
-                die("Error : " . $e->getMessage());
+        $cfg = Config::getInstance();
+        try {
+            switch ($type) {
+                case "mysql":
+                     return new \PDO("mysql:host=" . $cfg->get('db_hostname') . ";dbname=" . $cfg->get('db_name'), $cfg->get('db_user'), $cfg->get('db_password'));
+                default:
+                    throw new \PDOException("Database type not found");
             }
+        } catch (\PDOException $e) {
+            die("Error : " . $e->getMessage());
         }
-        return self::$_dbh;
     }
 }
