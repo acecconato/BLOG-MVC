c4792cfda234cf647881b839b941e4df2d0cde02
Anthony Cecconato <antho.cecc@gmail.com>
2018-03-19 13:38:35 +0100

    Bad path to config.php resolved

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,12 @@
 <?php
 
 return array(
+    // Database connection configuration
     "db_hostname"   => "localhost",
     "db_name"       => "perso_blog",
     "db_user"       => "root",
     "db_password"   => "",
 
-    "default_picture_path"  => dirname(__DIR__)."/public/image/posts"
+    "default_sql_type" => "mysql", // Default SQL type for creating database connections
+    "default_picture_posts_path"  => dirname(__DIR__, 2)."/public/image/posts" // Default path where pictures of posts are stocked
 );
