aecdb479187fd8dac776b1184ed4ec22aeab9756
Anthony Cecconato <antho.cecc@gmail.com>
2018-03-26 16:25:08 +0200

    Refactoring index

--- index.php
+++ index.php
@@ -6,29 +6,27 @@ use App\Router;
 
 $router = new Router($_GET['url']);
 
-$router->get("/", "Frontend#showHome"); // Index page
+$router->get("/", "Home#showHome"); // Index page
 
-$router->get("/articles/:id", "Frontend#getPost")->with(":id", "#[0-9]+#"); // Show the post :id
-$router->post("/articles/:id/", "Backend#addComment")->with(":id", "#[0-9]+#"); // Add a comment on the post :id
+$router->get("/articles/:id", "Blog#getPost")->with(":id", "#[0-9]+#"); // Show the post :id
+$router->post("/articles/:id/", "Blog#addComment")->with(":id", "#[0-9]+#"); // Add a comment on the post :id
 
-$router->get("/articles", "Frontend#getAllPosts"); // Show all posts
+$router->get("/articles", "Blog#getAllPosts"); // Show all posts
 
-$router->get("/connexion", "Frontend#loginInput"); // Login form
-$router->post("/connexion", "Backend#loginValidation"); // Login validation
+$router->get("/connexion", "App#loginForm"); // Login form
+$router->post("/connexion", "App#loginValidation"); // Login validation
 
-$router->get("/inscription", "Frontend#registerInput"); // Register form
-$router->post("/inscription", "Backend#registerValidation"); // Register validation
+$router->get("/admin/articles/supprimer/:id", "Backend#deletePost")->with(":id", "#[0-9]+#"); // Delete a post
 
-$router->get("/admin/articles/supprimer/:id", "Backend#addPost")->with(":id", "#[0-9]+#"); // Delete a post
-
-$router->get("/admin/articles/modifier/:id", "Backend#addPost")->with(":id", "#[0-9]+#"); // Form to modify a post
-$router->post("/admin/articles/modifier/:id", "Backend#addPost")->with(":id", "#[0-9]+#"); // Modify a post
+$router->get("/admin/articles/modifier/:id", "Backend#editPost")->with(":id", "#[0-9]+#"); // Form to modify a post
+$router->post("/admin/articles/modifier/:id", "Backend#addPost")->with(":id", "#[0-9]+#"); // Add a post
 
 $router->get("/admin/articles/ajouter", "Backend#addPost")->with(":id", "#[0-9]+#");  // Form to add a post
 $router->post("/admin/articles/ajouter", "Backend#addPost")->with(":id", "#[0-9]+#"); // Add a post
 
-$router->get("/admin/articles", "Backend#postsManagement"); // Posts management
-$router->get("/admin", "Backend#showAdminHome"); // Admin main panel
+$router->get("/admin/articles", "Backend#adminPosts"); // Posts management
+$router->get("/admin/commentaires", "Backend#adminComments"); // Comments management
+$router->get("/admin", "Backend#adminHome"); // Admin main panel
 
 try {
     $router->run();
