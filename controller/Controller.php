95c6d73a02f492c45d84c4f6077abc1c20b958da
Anthony Cecconato <antho.cecc@gmail.com>
2018-03-27 09:07:01 +0200

    Controller parent class created for auto generating views

---
+++ controller/Controller.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Controller;
+
+abstract class Controller
+{
+    protected $controllerName;
+
+    protected function generatePage($name, $args = [])
+    {
+        $view = strtolower($name);
+
+        if(!preg_match("#[\.]+#", $name)) {
+            $template = $this->controllerName = trim(preg_replace("#Controller#", "", $this->controllerName), "\\");
+            $template = $this->controllerName = strtolower($this->controllerName);
+        } else {
+            $nameExploded = explode(".", $name);
+
+            $template = $nameExploded[0];
+            $view = $nameExploded[1];
+        }
+
+        $templateToLoad = "/view/" . strtolower($template ) . ".php";
+        $viewToLoad = "/view/" . strtolower($view ) . "View.php";
+
+        ob_start();
+        extract($args);
+        require $viewToLoad;
+        $content = ob_get_clean();
+        require $templateToLoad;
+    }
+}
