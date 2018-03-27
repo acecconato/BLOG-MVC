6b80b83ae7dc0c3fa0a6e4531b3912c6c07c0530
Anthony Cecconato <antho.cecc@gmail.com>
2018-03-27 09:08:03 +0200

    WIP on Frontend controller

--- controller/FrontendController.php
+++ controller/FrontendController.php
@@ -2,29 +2,47 @@
 
 namespace Controller;
 
+use Model\Entities\Comment;
 use Model\Entities\Post;
 use Model\Managers\Manager;
-use Model\Managers\PostsManager;
 
-class FrontendController
+class FrontendController extends Controller
 {
-    private $posts = [],
-            $comments = [];
+    private $comments = [];
 
-    public static function showHome()
+    public function __construct()
     {
-        include dirname(__DIR__) . '/view/homeView.php';
+        $this->controllerName = __CLASS__;
     }
 
-    public function getPost($id)
+    public function showHome()
     {
-            if((int) $id > 0) {
-            /** @var PostsManager $postsManager */
-            $postsManager = Manager::getManagerOf("Posts");
-            $post = new Post($postsManager->getPostById($id));
+        $this->generatePage("home");
+    }
 
-            include dirname(__DIR__) . "/view/postView.php";
+    public function detailsOfPost($id)
+    {
+        /** @var \Model\Managers\PostsManager $postsManager */
+        $postsManager = Manager::getManagerOf("Posts");
+        /** @var \Model\Managers\CommentsManager $commentsManager */
+        $commentsManager = Manager::getManagerOf("Comments");
+
+        $postData = $postsManager->getPostById($id);
+
+        if(!is_array($postData)) {
+            header("Location: /articles");
         }
+
+        $post = new Post($postData);
+        $validatedComments = $commentsManager->getValidatedCommentsOfPost($post);
+
+        foreach ($validatedComments as $key => $data) {
+            $this->comments[] = new Comment($data);
+        }
+
+        $comments = $this->comments;
+
+        $this->generatePage("post", compact("post", "comments"));
     }
 
     public function addComment()
