35ccf1eb4c565ef3f5751b25a5d257f645950d59
Anthony Cecconato <antho.cecc@gmail.com>
2018-03-29 23:54:04 +0200

    Picture class created to manage images

--- app/Picture.php
+++ app/Picture.php
@@ -1,15 +1,39 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: antho
-     * Date: 29/03/2018
-     * Time: 19:10
-     */
 
     namespace App;
 
-
-    class Picture
+    abstract class Picture
     {
+        /**
+         * @param $file
+         * @return bool|string
+         * @throws \Exception
+         */
+        public static function getPostPicture($file)
+        {
+            $defaultPicturePath = Config::getInstance()->get("default_posts_picture");
+            $fullPath = ROOT . $defaultPicturePath . "/" . $file;
 
+            $pictureToLoad = implode(glob($fullPath.".*"));
+
+            $pictureToDisplay = explode("/", $pictureToLoad);
+            $pictureToDisplay = end($pictureToDisplay);
+
+            if(file_exists($pictureToLoad)) {
+                $fileType = exif_imagetype($pictureToLoad);
+                $allowedTypes = Config::getInstance()->getAllowedPostsImgType();
+
+                if(in_array($fileType, $allowedTypes)) {
+                    return $defaultPicturePath . "/" . $pictureToDisplay;
+                } else {
+                    throw new \Exception("Image type is not allowed");
+                }
+            }
+            return false;
+        }
+
+        public static function addNewPicture($picture)
+        {
+
+        }
     }
