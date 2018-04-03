7130cee1f19e2e2fb40e3f7d060e722d37d32ea1
Unknown <antho.cecc@gmail.com>
2018-04-03 23:08:00 +0200

    Creation of a Helper to have less code in the controller

--- app/Helper.php
+++ app/Helper.php
@@ -1,15 +1,37 @@
 <?php
-    /**
-     * Created by PhpStorm.
-     * User: antho
-     * Date: 03/04/2018
-     * Time: 19:27
-     */
-
     namespace App;
-
 
     class Helper
     {
+        public static function secureData(array $data)
+        {
+            $vars = [];
+            foreach ($data as $k => $v) {
+                $vars[$k] = trim(strip_tags($v));
+            }
 
+            return $vars;
+        }
+
+        public static function verifyComment($commentData)
+        {
+            $pseudo = $commentData["author"];
+            $message = $commentData["content"];
+
+            $err = [];
+
+            if(!isset($pseudo) || empty($pseudo) || !isset($message) || empty($message)) {
+                $err["warning"] = "Tous les champs ne sont pas remplis";
+            }
+
+            if(strlen($pseudo) > 50) {
+                $err["warning"] = "Le pseudo ne doit pas dépasser 50 caractères";
+            }
+
+            if(!empty($err)) {
+                return $err;
+            }
+
+            return true;
+        }
     }
